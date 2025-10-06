f1342444251ecdcc57ae8697a1e96dca433327c3
danielduka-dot <daniel.duka@skole.hr>
2025-10-06 13:53:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int n=0;
+int z = 0;
+int zbroj=0;
+
+Console.WriteLine("Unesite broj n: ");
+n = int.Parse(Console.ReadLine());
+
+/*for(int i=0; i<=n; i++)
+{
+    zbroj += i;
+}
+Console.WriteLine("Ukupni Fibonacijev zbroj je: {0}" , zbroj);*/
+do
+{
+    z++;
+    if (z % 2 == 0)
+    {
+        zbroj += z;
+    }
+} while (z < n);
+Console.WriteLine("Ukupni Fibonacijev zbroj je: {0}", zbroj);
